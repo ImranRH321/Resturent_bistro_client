191685a6d86cf36bb02b4d75dff62121bd3fde4a
ImranRH321 <imranrh247@gmail.com>
2023-07-05 00:54:19 +0600

    API to save Cart

--- src/pages/OrderFoodShop/FoodCard/FoodCard.jsx
+++ src/pages/OrderFoodShop/FoodCard/FoodCard.jsx
@@ -1,12 +1,53 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../../../context/AuthProvider/AuthProvider";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
 const FoodCard = ({ foodItem }) => {
   const { name, recipe, image, category, price } = foodItem;
-  // console.log(name, image); 
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  // console.log(name, image);
   // TODO:   Akane Image paitace na //
   const handleAddToCartBox = (itemEat) => {
-      console.log('eat: --> ',  itemEat);
-  }
+    // console.log('eat: --> ',  itemEat);
+    alert("click");
+    if (user) {
+      fetch("http://localhost:5000/carts", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(itemEat),
+      })
+        .then((res) => res.json())
+        .then((result) => {
+          // console.log(result, 'result');
+          if (result.insertedId) {
+            Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: "Your work has been saved",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          }
+        });
+    } else {
+      Swal.fire({
+        title: "Are you sure?",
+        text: "please to login to order page",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "login",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate("/login");
+        }
+      });
+    }
+  };
   return (
     <div className="card border-t-3 bg-base-100 shadow-xl">
       <figure>
@@ -26,7 +67,12 @@ const FoodCard = ({ foodItem }) => {
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions">
-          <button onClick={()=>handleAddToCartBox(foodItem)} className="btn btn-sm btn-outline border-b-orange-300 py-2">add to cart</button>
+          <button
+            onClick={() => handleAddToCartBox(foodItem)}
+            className="btn btn-sm btn-outline border-b-orange-300 py-2"
+          >
+            add to cart
+          </button>
         </div>
       </div>
     </div>
