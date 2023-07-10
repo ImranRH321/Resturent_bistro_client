51d5090c095fb5aee90cbffe9753ad3225840f50
ImranRH321 <imranrh247@gmail.com>
2023-07-10 15:57:54 +0600

    Stripe account and use publishable key

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import SectionTitle from "../../Shared/SectionTitle/SectionTitle";
+import CheckoutForm from "../CheckoutForm/CheckoutForm";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_pk);
 
 const Payment = () => {
   return (
@@ -9,6 +14,10 @@ const Payment = () => {
         largeHeading={"Payment Now"}
       ></SectionTitle>
       <h1 className="text-4xl">Teka teka tui oira Oira aso </h1>
+      {/*  */}
+      <Elements stripe={stripePromise}>
+        <CheckoutForm></CheckoutForm>
+      </Elements>
     </div>
   );
 };
