6614641e609f46a10e0ae0d95419961cfefbff42
ImranRH321 <imranrh247@gmail.com>
2023-07-11 12:35:49 +0600

    Create Admin dashboard stats api and load data

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
 
 const AdminHome = () => {
   const { user } = useAuth();
+  const { apis } = useAxiosSecure();
+
+  const {} = useQuery({
+    queryKey: ["/admin/stats"],
+    queryFn: async () => {
+      const res = await apis("/admin/stats");
+      console.log(res.data,' admin home');
+      return res.data;
+    },
+  });
+
   return (
     <div className="w-full">
-      <h1 className="text-4xl">WelCome Admin:  {user.displayName} </h1>
+      <h1 className="text-4xl">WelCome Admin: {user.displayName} </h1>
     </div>
   );
 };
--- src/pages/Dashboard/UserHome/UserHome.jsx
+++ src/pages/Dashboard/UserHome/UserHome.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const UserHome = () => {
+    
     return (
         <div>
             <h1>user home </h1>
